4f730f4a22c2dfb4c436d20645d94749f9284a22
claudio afaro <claudioalfaro745@gmail.com>
2024-04-01 22:43:02 -0300

    scraper steam

--- Quinta Region/scraper.py
+++ Quinta Region/scraper.py
@@ -1,9 +1,38 @@
 import requests
+from bs4 import BeautifulSoup
+import re
 
-web = "https://www.portaltransparencia.cl/PortalPdT/directorio-de-organismos-regulados/?org=MU332"
+# Parte 1, obtener el HTML.
 
-result = requests.get(web)
+web = "https://store.steampowered.com/search/?filter=topsellers"
+pedido = requests.get(web)
+html = pedido.text
 
-conten = result.text
 
-print(conten)
+# Parte 2, "Parsear" el HTML.
+
+
+soup = BeautifulSoup(html, "html.parser")
+
+
+#Parte 3, Obtener los datos necesarios.
+
+
+div_completo = soup.find_all('div', class_ = "col search_name ellipsis")
+
+# fecha lanzamiento: col search_released responsive_secondrow
+# precio: col search_price_discount_combined responsive_secondrow
+
+producto = []
+lanzamiento = []
+precio = []
+
+for div in div_completo:
+    producto.append(div.text.strip())
+
+print(producto)
+
+
+
+
+
